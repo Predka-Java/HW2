77be446b12ee75ceb822fcd21416a861074bda95
Predka <swaggajugga69@gmail.com>
2022-12-16 21:30:02 +0700

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на выходе  показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+int res = (a /10) % 10;
+Console.WriteLine(res);
